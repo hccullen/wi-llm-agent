4c5e9331fc46a1771eccaef9990711d5402cac5d
Henrik Cullen <hccullen@users.noreply.github.com>
2024-09-03 09:48:43 +0200

    More

--- src/controllers/extractQualifierController.ts
+++ src/controllers/extractQualifierController.ts
@@ -4,18 +4,14 @@ import { identifyQualifiers } from '../handlers/identifyQualifiers';
 
 
 const handleExtractQualifiers = async (req: Request & { file?: any }, res: Response) => {
-  console.log(req.file);
-  // if (!req.file || req.file.mimetype !== 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
-  //   return res.status(400).json({ message: "Invalid file type" });
-  // }
 
+  if (!req.file || req.file.mimetype !== 'application/pdf') {
+    return res.status(400).json({ message: "Invalid file type" });
+  }
 
   try {
     const fileBlob = new Blob([req.file.buffer], { type: req.file.mimetype });
-
-    // Assuming identifyQualifiers can accept a Buffer
-    const extractedQualifiers = await identifyQualifiers(fileBlob, req.file.path);
-
+    const extractedQualifiers = await identifyQualifiers(fileBlob);
     return res.status(200).json(extractedQualifiers);
   } catch (error) {
     return res.status(500).json({ message: "Error processing file", error });
